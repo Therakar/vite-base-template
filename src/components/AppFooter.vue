fd310ce8ad3df520256596e149eb16e50a9506ef
Therakar <matteo.pau200@gmail.com>
2022-11-24 14:08:18 +0100

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'AppFooter',
+}
+</script>
+
+<template>
+  <h1>FOOTER</h1>
+</template>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<template>
+  <h1>HEADER</h1>
+</template>
+
+<style>
+</style>

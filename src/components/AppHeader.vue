ad95bb18b9259c2ab10adb6eca7f36ca095f69f4
Therakar <matteo.pau200@gmail.com>
2022-11-24 16:20:53 +0100

    inserimento del nav nell'header utilizzando v-for

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,25 @@
 <!-- HEADER -->
 <script>
 import AppIcon from './AppIcon.vue';
+import AppHeaderNav from './AppHeaderNav.vue';
 export default {
   name: 'AppHeader',
   components: {
     AppIcon,
-  }
+    AppHeaderNav,
+  },
 }
 </script>
 
 <template>
-  <header class="container">
+  <header class="container flex sp-bw al-cnt">
     <AppIcon/>
+    <AppHeaderNav/>
   </header>
 </template>
 
 <style scoped>
   .container{
-    padding: 20px;
+    padding: 10px;
     }
 </style>
---
+++ src/components/AppHeaderNav.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'AppHeaderNav',
+  data() {
+    return {
+        links: [
+            'CHARACTERS',
+            'COMICS',
+            'MOVIES',
+            'TV',
+            'GAMES',
+            'COLLECTIBLES',
+            'VIDEOS',
+            'FANS',
+            'NEWS',
+            'SHOP'
+        ]
+    }
+  }
+}
+</script>
+
+<template>
+  <nav>
+    <ul class="flex">
+        <li v-for="link in links"><a href="">{{link}}</a></li>
+    </ul>
+</nav>
+</template>
+
+<style lang="scss" scoped>
+  ul{
+    list-style: none;
+
+    a{
+        display: inline-block;
+        padding: 15px 15px;
+        text-decoration: none;
+        color: #000;
+    }
+  }
+</style>
